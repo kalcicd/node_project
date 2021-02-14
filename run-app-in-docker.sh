8c216d873c407b3ce14aaa19bbd049f0c1498aa9
kerrmat <kerrmat@oregonstate.edu>
2021-02-14 13:12:27 -0800

    removed sudo from run app in docker so it works on windows

--- run-app-in-docker.sh
+++ run-app-in-docker.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 # Forces running containers to stop.
-sudo docker-compose kill
+docker-compose kill
 
 # Forcefully removes any stopped service containers.
-sudo docker-compose rm -f
+docker-compose rm -f
 
-sudo docker-compose build
+docker-compose build
 
 # Runs the app with port(s) enabled and mapped to the host. Removes the container after run.
-sudo docker-compose run --rm --service-ports app
+docker-compose run --rm --service-ports app
