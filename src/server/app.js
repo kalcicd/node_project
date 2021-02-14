0b0f681fef91aa6a7de75bfc1f59876bd1a9f322
David Kalcic <kalcicd@oregonstate.edu>
2021-02-14 12:56:02 -0800

    Merge master, update search bar

--- src/server/app.js
+++ src/server/app.js
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import fs from 'fs'
 import path from 'path'
 import express from 'express'
@@ -12,7 +13,6 @@ import Error404 from '../client/components/404'
 import Location from '../client/components/location'
 import Login from '../client/components/login'
 import Verify from '../client/components/verify'
-import Volunteer from '../client/components/volunteer'
 import Officeholder from '../client/components/officeholder'
 
 import config from '../../config/default.json'
@@ -20,169 +20,172 @@ import config from '../../config/default.json'
 const pathToTemplate = path.join(__dirname, './views/layout.html')
 const template = fs.readFileSync(pathToTemplate, 'utf8')
 
-const session = require('express-session');
+const session = require('express-session')
 
-const bcrypt = require('bcrypt');
-const bcryptSaltRounds = 15;
+const bcrypt = require('bcrypt')
+const bcryptSaltRounds = 15
 
-const app = express();
-//use body-parser to handle post requests
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(bodyParser.json());
+const app = express()
+// use body-parser to handle post requests
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.use(express.static('./src/server/public'))
 
-//setup session secret
-const sessionSecret = String(fs.readFileSync('./secret.txt')).replace("\n","");
+// setup session secret
 app.use(session({
-  secret:sessionSecret,
+  secret: config.sessionSecret.secret,
   resave: false,
   saveUninitialized: false,
   cookie: {}
-}));
+}))
 
-
-function userLoginStatus(req){
-  //extracts the relevant user data from the active session and returns it in a dict
-  let statusDict = {"logged_in":false,"is_verifier":false}
-  if(req.session === undefined || req.session.user === undefined){
-    return statusDict;
+function userLoginStatus (req) {
+  // extracts the relevant user data from the active session and returns it in a dict
+  let statusDict = { 'logged_in': false, 'is_verifier': false }
+  if (req.session === undefined || req.session.user === undefined) {
+    return statusDict
   }
-  statusDict["logged_in"] = true;
-  statusDict["username"] = req.session.user;
-  if(req.session.is_verifier === undefined || req.session.is_verifier === false){
-    return statusDict;
+  statusDict['logged_in'] = true
+  statusDict['username'] = req.session.user
+  if (req.session.is_verifier === undefined || req.session.is_verifier === false) {
+    return statusDict
   }
-  statusDict["is_verifier"]=true;
-  return statusDict;
+  statusDict['is_verifier'] = true
+  return statusDict
 }
 
 app.get('/', (req, res, next) => {
-  let userStatus = userLoginStatus(req);
+  let userStatus = userLoginStatus(req)
   const renderedContent = renderToString(
-    <Index logged_in={userStatus.logged_in} is_verifier={userStatus.is_verifier}/>
-  );
-  let page = template.replace('<!-- CONTENT -->', renderedContent);
-  page = page.replace('<!-- STYLESHEET -->', '/css/index.css');
-  res.status(200).send(page);
+    <Index logged_in={userStatus.logged_in} is_verifier={userStatus.is_verifier} />
+  )
+  let page = template.replace('<!-- CONTENT -->', renderedContent)
+  page = page.replace('<!-- STYLESHEET -->', '/css/index.css')
+  res.status(200).send(page)
 })
 app.get('/about', (req, res, next) => {
-  let userStatus = userLoginStatus(req);
+  let userStatus = userLoginStatus(req)
   const renderedContent = renderToString(
-    <AboutUs logged_in={userStatus.logged_in} is_verifier={userStatus.is_verifier}/>
-  );
-  let page = template.replace('<!-- CONTENT -->', renderedContent);
-  page = page.replace('<!-- STYLESHEET -->', '/css/aboutus.css');
-  res.status(200).send(page);
+    <AboutUs logged_in={userStatus.logged_in} is_verifier={userStatus.is_verifier} />
+  )
+  let page = template.replace('<!-- CONTENT -->', renderedContent)
+  page = page.replace('<!-- STYLESHEET -->', '/css/aboutus.css')
+  res.status(200).send(page)
 })
 app.get('/volunteer', (req, res, next) => {
-  let userStatus = userLoginStatus(req);
+  let userStatus = userLoginStatus(req)
   const renderedContent = renderToString(
-    <Volunteer logged_in={userStatus.logged_in} is_verifier={userStatus.is_verifier}/>
-  );
-  let page = template.replace('<!-- CONTENT -->', renderedContent);
-  page = page.replace('<!-- STYLESHEET -->', '/css/volunteer.css');
-  res.status(200).send(page);
+    <Volunteer logged_in={userStatus.logged_in} is_verifier={userStatus.is_verifier} />
+  )
+  let page = template.replace('<!-- CONTENT -->', renderedContent)
+  page = page.replace('<!-- STYLESHEET -->', '/css/volunteer.css')
+  res.status(200).send(page)
 })
 app.get('/developers', (req, res, next) => {
-  let userStatus = userLoginStatus(req);
+  let userStatus = userLoginStatus(req)
   const renderedContent = renderToString(
-    <Developers logged_in={userStatus.logged_in} is_verifier={userStatus.is_verifier}/>
-  );
-  let page = template.replace('<!-- CONTENT -->', renderedContent);
-  page = page.replace('<!-- STYLESHEET -->', '/css/developers.css');
-  res.status(200).send(page);
+    <Developers logged_in={userStatus.logged_in} is_verifier={userStatus.is_verifier} />
+  )
+  let page = template.replace('<!-- CONTENT -->', renderedContent)
+  page = page.replace('<!-- STYLESHEET -->', '/css/developers.css')
+  res.status(200).send(page)
 })
 
-//show a selection of unverified submissions
+// show a selection of unverified submissions
 app.get('/verify', (req, res, next) => {
-  let userStatus = userLoginStatus(req);
-  if(userStatus.is_verifier!==true){
-    //redirect the user back to the landing page if they are not a verifier
-    res.redirect(403,"/");
-    return;
+  let userStatus = userLoginStatus(req)
+  if (userStatus.is_verifier !== true) {
+    // redirect the user back to the landing page if they are not a verifier
+    res.redirect(403, '/')
+    return
   }
-  //todo: fetch submissions from database
-  let submissions=[ //temporary submissions list
+  // todo: fetch submissions from database
+  let submissions = [ // temporary submissions list
     {
-      "title":"Corvalis","type":"location","reference":"https://en.wikipedia.org","id":0,
-    "updates":[["Location","Corvalis","Corvallis"]]
+      'title': 'Corvalis',
+      'type': 'location',
+      'reference': 'https://en.wikipedia.org',
+      'id': 0,
+      'updates': [['Location', 'Corvalis', 'Corvallis']]
     },
     {
-      "title":"Corvallis City Council Member","type":"office","reference":"https://en.wikipedia.org",
-      "id":0,"updates":[["OfficeTitle","City Council Member","City Councilor"]]
+      'title': 'Corvallis City Council Member',
+      'type': 'office',
+      'reference': 'https://en.wikipedia.org',
+      'id': 0,
+      'updates': [['OfficeTitle', 'City Council Member', 'City Councilor']]
     }
   ]
   const renderedContent = renderToString(<Verify
     submissions={submissions} logged_in={userStatus.logged_in} is_verifier={userStatus.is_verifier}
-  />);
-  let page = template.replace('<!-- CONTENT -->',renderedContent);
-  page = page.replace('<!-- STYLESHEET -->', '/css/verify.css');
-  page = page.replace('<!--SCRIPT-->','<script src="/js/verify.js" defer></script>');
-  res.status(200).send(page);
+  />)
+  let page = template.replace('<!-- CONTENT -->', renderedContent)
+  page = page.replace('<!-- STYLESHEET -->', '/css/verify.css')
+  page = page.replace('<!--SCRIPT-->', '<script src="/js/verify.js" defer></script>')
+  res.status(200).send(page)
 })
 
-//User login page
-app.get('/login',(req,res,next)=>{
+// User login page
+app.get('/login', (req, res, next) => {
   let showLoginError = (req.query.verificationFailed !== undefined)
-  const renderedContent = renderToString(<Login/>);
-  let page = template.replace('<!-- CONTENT -->',renderedContent);
-  page = page.replace('<!-- STYLESHEET -->', '/css/login.css');
-  //page = page.replace('<!--SCRIPT-->','<script src="/js/verify.js" defer></script>');
-  res.status(200).send(page);
+  const renderedContent = renderToString(<Login />)
+  let page = template.replace('<!-- CONTENT -->', renderedContent)
+  page = page.replace('<!-- STYLESHEET -->', '/css/login.css')
+  // page = page.replace('<!--SCRIPT-->','<script src="/js/verify.js" defer></script>');
+  res.status(200).send(page)
 })
-//Handle user login submission
-app.post('/login',(req,res,next)=>{
-  //todo: fetch hash from database based on username
-  //temporary credentials: username: user (or user2), password: password
+// Handle user login submission
+app.post('/login', (req, res, next) => {
+  // todo: fetch hash from database based on username
+  // temporary credentials: username: user (or user2), password: password
   const temporaryUsersTable = {
-    "user":{password:"$2b$15$XilY7gUBSvL7l4Yh5uFXkuwYXTV4u9ikB0OvKRxqq5fNWrpi17VTS",is_verifier:true},
-    "user2":{password:"$2b$15$XilY7gUBSvL7l4Yh5uFXkuwYXTV4u9ikB0OvKRxqq5fNWrpi17VTS",is_verifier:false}
+    'user': { password: '$2b$15$XilY7gUBSvL7l4Yh5uFXkuwYXTV4u9ikB0OvKRxqq5fNWrpi17VTS', is_verifier: true },
+    'user2': { password: '$2b$15$XilY7gUBSvL7l4Yh5uFXkuwYXTV4u9ikB0OvKRxqq5fNWrpi17VTS', is_verifier: false }
   }
-  //function to be called if the user's credentials are rejected
-  function loginFailed(){
-    res.redirect(403,"/login?verificationFailed=true");
+
+  // function to be called if the user's credentials are rejected
+  function loginFailed () {
+    res.redirect(403, '/login?verificationFailed=true')
   }
-  let username = req.body.user;
-  if(temporaryUsersTable.hasOwnProperty(username)){
+
+  let username = req.body.user
+  if (temporaryUsersTable.hasOwnProperty(username)) {
     bcrypt.compare(
       req.body.pass,
       temporaryUsersTable[username].password,
-      function(err,result){
-        if(err === undefined){ //no errors occurred
-          if(result){ //login was successful
-            req.session.user = username;
-            req.session.is_verifier = temporaryUsersTable[username].is_verifier;
-            res.redirect("/");
+      function (err, result) {
+        if (err === undefined) { // no errors occurred
+          if (result) { // login was successful
+            req.session.user = username
+            req.session.is_verifier = temporaryUsersTable[username].is_verifier
+            res.redirect('/')
+          } else { // login failed
+            loginFailed()
           }
-          else{ //login failed
-            loginFailed();
-          }
-        }
-        else{ //an error occurred
-          res.status(500).send("");
+        } else { // an error occurred
+          res.status(500).send('')
         }
       }
-    );
+    )
   }
 })
-//handle user logout
-app.get('/logout',(req,res,next)=>{
-  req.session.destroy(function (err){
-    if(!err){ //no errors occurred
-      res.redirect(302,'/');
+// handle user logout
+app.get('/logout', (req, res, next) => {
+  req.session.destroy(function (err) {
+    if (!err) { // no errors occurred
+      res.redirect(302, '/')
+    } else { // an error occurred while trying to logout
+      res.redirect(302, '/?logoutFailed=true')
     }
-    else{ //an error occurred while trying to logout
-      res.redirect(302,'/?logoutFailed=true');
-    }
-  });
+  })
 })
 
 // Generate Results page
 app.get('/location', (req, res, next) => {
   const { lat, lon } = req.query
-  const renderedContent = renderToString(React.createElement(Location, {lat, lon}))
+  const renderedContent = renderToString(React.createElement(Location, { lat, lon }))
   let page = template.replace('<!-- CONTENT -->', renderedContent)
   page = page.replace('<!-- STYLESHEET -->', '/css/location.css')
   res.status(200).send(page)
